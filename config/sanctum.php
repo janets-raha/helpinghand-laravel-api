06b0857bf8db5b816b51e9a221f8a10e4c40c068
janets-raha <jeannette.cecora@gmail.com>
2020-08-18 15:25:40 +0200

    add URL to sanctum config

--- config/sanctum.php
+++ config/sanctum.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost, localhost:8080, 127.0.0.1,127.0.0.1:8000,::1')),
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'inspiring-mahavira-69e3af.netlify.app', 'helpinghand-laravel.herokuapp.com', 'localhost, localhost:8080, 127.0.0.1,127.0.0.1:8000,::1')),
 
     /*
     |--------------------------------------------------------------------------
